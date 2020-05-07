29f0c222f78fd9892d4344f48f0a7dc128171384
Arthur <arthurnunes.petee@gmail.com>
2020-05-07 20:06:52 -0300

    detalhes versao 2020

--- ref/js/sidenav.js
+++ ref/js/sidenav.js
@@ -1,20 +1,18 @@
 function toggleNav() {
     mySideNav = document.querySelector('#mySidenav')
     if (mySideNav.style.width == '0px'){
-        document.getElementById("mySidenav").style.width = "15%";
-        document.querySelector("#rodape").style.marginLeft = "-22%";
-        document.querySelector("html").style.marginLeft = "22%";
-        document.querySelector('#sidenavToggler').style.marginLeft = "15%";
+        document.getElementById("mySidenav").style.width = "25vmax";
+        document.querySelector('#sidenavToggler').style.marginLeft = "25vmax";
         document.getElementById("sidenavTogglerIcon").classList.remove("fa-chevron-right");
         document.getElementById("sidenavTogglerIcon").classList.add("fa-chevron-left");
+        $('#blackmask').css('display', 'block');
     }
-    else{
-        document.getElementById("mySidenav").style.width = "0";
-        document.querySelector("#rodape").style.marginLeft = "-7%";
-        document.querySelector("html").style.marginLeft = "7%";
+    else{        
+        document.getElementById("mySidenav").style.width = "0px";
         document.querySelector('#sidenavToggler').style.marginLeft = "0";
         document.getElementById("sidenavTogglerIcon").classList.remove("fa-chevron-left");
         document.getElementById("sidenavTogglerIcon").classList.add("fa-chevron-right");
+        $('#blackmask').css('display', 'none');
     }
     
   }
