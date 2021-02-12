2b3a5126e510ba1efc242dedb2a3e28299c6362d
Clara Martins <cmcm11320@gmail.com>
2021-02-11 23:51:21 -0300

    Privacy Policy Page Creation
    criação de pagina de política de privacidade

--- ref/js/main.js
+++ ref/js/main.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip();   
 });
+
+// close button cookie pop up
+$(".close").click(function() {
+	$(".cookie-banner").css("display","none");
+})
+
+function cookietimer(){
+	$(".cookie-banner").css("display","none");
+	localStorage.setItem("cookieSeen","sim")
+}
